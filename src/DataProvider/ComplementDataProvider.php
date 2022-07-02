cae4588af8f68d36fda4bcf9d403db1df73f140b
SIDY SAMAKE SAMBOU <samakesamboudysi@gmail.com>
2022-07-02 22:19:50 +0000

    pagination

--- src/DataProvider/ComplementDataProvider.php
+++ src/DataProvider/ComplementDataProvider.php
@@ -1,29 +1,34 @@
 <?php
-// api/src/DataProvider/BlogPostCollectionDataProvider.php
-
 namespace App\DataProvider;
-use App\Repository\MenuRepository;
-use App\Repository\BurgerRepository;
-use App\Entity\Complements;
+
+use App\Entity\BlogPost;
+use App\Entity\Complement;
+use App\Repository\TailleRepository;
+use App\Repository\PortionFriteRepository;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
 
 final class ComplementDataProvider implements ContextAwareCollectionDataProviderInterface, RestrictedDataProviderInterface
-{    
-    private $menuRepo;
-    private $burgerRepo;
-    public function __construct(MenuRepository $menuRepo, BurgerRepository $burgerRepo)
-    {
-        
-    }
+{
+    private $portionfriteRepo;
+    private $tailleRepo;
 
+    public function __construct(PortionFriteRepository $portionfriteRepo,TailleRepository $tailleRepo)
+    {
+        $this->portionfriteRepo=$portionfriteRepo;
+        $this->tailleRepo=$portionfriteRepo;
+    }
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
     {
-        return  ComplementDataProvider ::class === $resourceClass;
+        return Complement::class === $resourceClass;
     }
 
     public function getCollection(string $resourceClass, string $operationName = null, array $context = [])
     {
-        
+        $complements = [];
+        [$complements="portionfrite"=> $this->portionfriteRepo->findAll()];
+        [$complements="taille"=> $this->tailleRepo->findAll()];
+
+        return $complements;
     }
 }
