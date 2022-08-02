27e7ef50646adad54d52ab0dcd655a6f5e3a40a0
sidy samaké <s100make@gmail.com>
2022-08-02 09:05:46 +0000

    dfff

--- src/Entity/Details.php
+++ src/Entity/Details.php
@@ -9,7 +9,6 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
 #[ApiResource(
-    // collectionOperations:[],
     itemOperations: ["get"=>[
         "normalization_context"=>["groups"=>["details"]]]]
         
